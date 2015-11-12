10708f1e644de3aaeb16b66e63056ba1f9a1deb8
Persilolka <persilolka@gmail.com>
2015-11-13 00:17:42 +0200

    Change Professor.cs

--- Entities/Professor.cs
+++ Entities/Professor.cs
@@ -8,5 +8,22 @@ namespace Entities
 {
     public class Professor
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //need dynamic collections, but now let be static array
+        public Classes[] CanTeach = new Classes[5];
+
+        public Professor(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public Professor(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
     }
 }
