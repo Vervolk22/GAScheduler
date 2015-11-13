c9e28c69c19f84c7acddffde3e2926f09f18805d
Persilolka <persilolka@gmail.com>
2015-11-13 02:23:31 +0200

    modifying Classes.cs, delete Day.cs

--- Entities/Classes.cs
+++ Entities/Classes.cs
@@ -8,5 +8,29 @@ namespace Entities
 {
     class Classes
     {
+        public Week Week { get; set; }
+        public ClassTime Time { get; set; }
+        public DayOfWeek Day { get; set; }
+        public Audience Audience { get; set; }
+        public Group Group { get; set; }
+        public Professor Professor { get; set; }
+        public Subject Subject { get; set; }
+
     }
+
+
+    //I don't know how to do this enum...
+    enum ClassTime
+    {
+        
+    }
+
+    enum Week
+    {
+        First = 1,
+        Second = 2,
+        Third = 3,
+        Fourth = 4
+    }
+
 }
