4417c3cb4b906c9ae41f2e8547039f991fcbdcf2
Andrei Zholudzeu <Bozon22@gmail.com>
2015-11-21 23:34:02 +0300

    Added carcass of RandomScheduleGenerator

--- GA/Algorithm.cs
+++ GA/Algorithm.cs
@@ -4,32 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities;
+using DAL;
 
 namespace GA
 {
     public class Algorithm
     {
         List<Schedule> population;
+        List<Group> groups;
+        List<Curriculum> curriculum;
+        List<Audience> audiences;
+        List<Professor> professors;
 
         public Algorithm()
         {
             population = new List<Schedule>();
+            IDataProvider dataProvider = DataProviderFactory.getTestDataProvider();
+            groups = dataProvider.getGroups();
+            curriculum = dataProvider.getCurriculum();
+            audiences = dataProvider.getAudiences();
+            professors = dataProvider.getProfessors();
         }
 
         public void generateInitialPopulation(int count)
         {
+            RandomScheduleGenerator rnd = new RandomScheduleGenerator(groups,
+                    curriculum, audiences, professors);
             for (int i = 0; i < count; i++)
             {
-                population.Add(getRandomIndividual());
+                population.Add(rnd.getRandomSchedule(i));
             }
-        }
-
-        protected Schedule getRandomIndividual(int scheduleId)
-        {
-            Random rnd = new Random();
-            List<Classes> classes = new List<Classes>();
-            // TODO fill classes list
-            Schedule schedule = new Schedule(scheduleId, classes);
         }
     }
 }
---
+++ GA/RandomScheduleGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace GA
+{
+    class RandomScheduleGenerator
+    {
+        List<Group> groups;
+        List<Curriculum> curriculum;
+        List<Audience> audiences;
+        List<Professor> professors;
+
+        internal RandomScheduleGenerator(List<Group> groups, List<Curriculum> curriculum,
+                List<Audience> audiences, List<Professor> professors)
+        {
+            this.groups = groups;
+            this.curriculum = curriculum;
+            this.audiences = audiences;
+            this.professors = professors;
+        }
+
+        internal Schedule getRandomSchedule(int scheduleId)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
