b8675ddefc77e8f49d8c9494d2c8613f6a429632
Andrei Zholudzeu <Bozon22@gmail.com>
2015-11-21 16:21:39 +0300

    Added a bit of code of algorithm

--- GA/Algorithm.cs
+++ GA/Algorithm.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace GA
 {
     public class Algorithm
     {
+        List<Schedule> population;
+
+        public Algorithm()
+        {
+            population = new List<Schedule>();
+        }
+
+        public void generateInitialPopulation(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                population.Add(getRandomIndividual());
+            }
+        }
+
+        protected Schedule getRandomIndividual(int scheduleId)
+        {
+            Random rnd = new Random();
+            List<Classes> classes = new List<Classes>();
+            // TODO fill classes list
+            Schedule schedule = new Schedule(scheduleId, classes);
+        }
     }
 }
