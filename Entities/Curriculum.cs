775579cfe2d23befe7b4254204d8d0ea3b6c6957
Andrei Zholudzeu <Bozon22@gmail.com>
2015-11-22 11:24:04 +0300

    Changes in Curriculum class to GA requirements

--- Entities/Curriculum.cs
+++ Entities/Curriculum.cs
@@ -10,13 +10,23 @@ namespace Entities
     {
         public int id { get; private set; }
         public string name { get; private set; }
-        public List<SubjectCurriculum> curriculum { get; private set; }
+        public LinkedList<SubjectCurriculum> curriculum { get; private set; }
 
-        public Curriculum(int id, string name, List<SubjectCurriculum> curriculum)
+        public Curriculum(int id, string name, LinkedList<SubjectCurriculum> curriculum)
         {
             this.id = id;
             this.name = name;
             this.curriculum = curriculum;
+        }
+
+        public static Curriculum copy(Curriculum original)
+        {
+            LinkedList<SubjectCurriculum> newList = new LinkedList<SubjectCurriculum>();
+            foreach (SubjectCurriculum sc in original.curriculum)
+            {
+                newList.AddLast(SubjectCurriculum.copy(sc));
+            }
+            return new Curriculum(original.id, original.name, newList);
         }
     }
 }
